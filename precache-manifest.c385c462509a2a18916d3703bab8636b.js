7b44bf27589e58e643f1eb4bde618859faeb0640
Zekun-Yang <zczlzy2@ucl.ac.uk>
2019-11-18 18:44:08 +0000

    Updates

---
+++ precache-manifest.c385c462509a2a18916d3703bab8636b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58b37f854be74bfa74be350047435874",
+    "url": "/comp0016-team8/index.html"
+  },
+  {
+    "revision": "024e1b319b0bbfd0444e",
+    "url": "/comp0016-team8/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "f6e433efea942bdc156c",
+    "url": "/comp0016-team8/static/js/2.b3090d7e.chunk.js"
+  },
+  {
+    "revision": "024e1b319b0bbfd0444e",
+    "url": "/comp0016-team8/static/js/main.7f0dacbb.chunk.js"
+  },
+  {
+    "revision": "61e98cc260fce96d62ef",
+    "url": "/comp0016-team8/static/js/runtime-main.d18c538d.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/comp0016-team8/static/media/logo.25bf045c.svg"
+  }
+]);
